64a956b359003ec0c5b1676440b8e1a9a0508291
Jello <jello.aka.liu@foxmail.com>
2023-12-12 21:10:58 +0800

    feat: 移除 RTK, redux

--- src/localRedux/localStore.ts
+++
@@ -1,4 +0,0 @@
-import createStoreLocalRedux from "@/localRedux/index";
-import { counterReducer } from "@/feature/counter";
-
-export const localStore = createStoreLocalRedux(counterReducer as any);
--- src/state/store.ts
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore } from 'redux';
-import { counterReducer } from "@/feature/counter";
-
-const store = createStore(counterReducer);
-
-export default store;
